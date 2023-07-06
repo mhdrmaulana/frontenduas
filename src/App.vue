ecb4aed0c4407d6439916a9a478473110ec8e1f0
mhdrmaulana <129238875+mhdrmaulana@users.noreply.github.com>
2023-07-06 16:56:59 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,20 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="/">Buku</a>
-            <a class="nav-link" href="/kategori">Kategori</a>
-            <a class="nav-link" href="/anggota">Anggota</a>
-            <a class="nav-link" href="/peminjaman">Peminjaman</a>
-            <a class="nav-link" href="/peminjaman-master">Peminjaman Master</a>
-          </div>
-        </div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Buku</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/kategori">Kategori</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/anggota">Anggota</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/peminjaman">Peminjaman</a>
+            </li>
+          </ul>
       </div>
     </nav>
   </header>
