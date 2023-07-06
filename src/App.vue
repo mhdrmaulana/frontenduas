eb0368008f6bf2aa12f0f313fcc61eefd01e7e92
mhdrmaulana <129238875+mhdrmaulana@users.noreply.github.com>
2023-07-06 17:01:39 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,14 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Buku</a>
+              <a class="nav-link" href="/">Buku</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/kategori">Kategori</a>
@@ -20,6 +25,7 @@ import { RouterLink, RouterView } from 'vue-router'
               <a class="nav-link" href="/peminjaman">Peminjaman</a>
             </li>
           </ul>
+        </div>
       </div>
     </nav>
   </header>
